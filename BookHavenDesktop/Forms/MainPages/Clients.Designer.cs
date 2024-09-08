4c082e5320d147be107285aa7c0fff5d9d9702f2
Emily Heinz <531971@student.fontys.nl>
2024-09-08 17:14:38 +0200

    first design

--- BookHavenDesktop/Forms/MainPages/Clients.Designer.cs
+++ BookHavenDesktop/Forms/MainPages/Clients.Designer.cs
@@ -28,18 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            pnlClient = new Panel();
+            btnSearchClient = new Button();
+            txtEmailSearch = new TextBox();
+            txtLNameSearch = new TextBox();
+            txtFNameSearch = new TextBox();
+            lblEMail = new Label();
+            lblLName = new Label();
+            lblFname = new Label();
+            lbl1 = new Label();
+            lblCLients = new Label();
+            pnlClient.SuspendLayout();
             SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.White;
+            flowLayoutPanel1.Location = new Point(426, -1);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1201, 920);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // pnlClient
+            // 
+            pnlClient.BackColor = Color.White;
+            pnlClient.Controls.Add(btnSearchClient);
+            pnlClient.Controls.Add(txtEmailSearch);
+            pnlClient.Controls.Add(txtLNameSearch);
+            pnlClient.Controls.Add(txtFNameSearch);
+            pnlClient.Controls.Add(lblEMail);
+            pnlClient.Controls.Add(lblLName);
+            pnlClient.Controls.Add(lblFname);
+            pnlClient.Controls.Add(lbl1);
+            pnlClient.Location = new Point(24, 70);
+            pnlClient.Name = "pnlClient";
+            pnlClient.Size = new Size(355, 460);
+            pnlClient.TabIndex = 1;
+            // 
+            // btnSearchClient
+            // 
+            btnSearchClient.BackColor = Color.FromArgb(130, 76, 113);
+            btnSearchClient.FlatAppearance.BorderSize = 0;
+            btnSearchClient.FlatStyle = FlatStyle.Flat;
+            btnSearchClient.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSearchClient.ForeColor = Color.White;
+            btnSearchClient.Location = new Point(22, 389);
+            btnSearchClient.Name = "btnSearchClient";
+            btnSearchClient.Size = new Size(298, 50);
+            btnSearchClient.TabIndex = 20;
+            btnSearchClient.Text = "Search client";
+            btnSearchClient.UseVisualStyleBackColor = false;
+            // 
+            // txtEmailSearch
+            // 
+            txtEmailSearch.BackColor = Color.WhiteSmoke;
+            txtEmailSearch.BorderStyle = BorderStyle.None;
+            txtEmailSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtEmailSearch.Location = new Point(22, 318);
+            txtEmailSearch.Multiline = true;
+            txtEmailSearch.Name = "txtEmailSearch";
+            txtEmailSearch.Size = new Size(298, 41);
+            txtEmailSearch.TabIndex = 19;
+            // 
+            // txtLNameSearch
+            // 
+            txtLNameSearch.BackColor = Color.WhiteSmoke;
+            txtLNameSearch.BorderStyle = BorderStyle.None;
+            txtLNameSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtLNameSearch.Location = new Point(21, 206);
+            txtLNameSearch.Multiline = true;
+            txtLNameSearch.Name = "txtLNameSearch";
+            txtLNameSearch.Size = new Size(298, 41);
+            txtLNameSearch.TabIndex = 18;
+            // 
+            // txtFNameSearch
+            // 
+            txtFNameSearch.BackColor = Color.WhiteSmoke;
+            txtFNameSearch.BorderStyle = BorderStyle.None;
+            txtFNameSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtFNameSearch.Location = new Point(21, 110);
+            txtFNameSearch.Multiline = true;
+            txtFNameSearch.Name = "txtFNameSearch";
+            txtFNameSearch.Size = new Size(298, 41);
+            txtFNameSearch.TabIndex = 17;
+            // 
+            // lblEMail
+            // 
+            lblEMail.AutoSize = true;
+            lblEMail.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEMail.ForeColor = Color.Black;
+            lblEMail.Location = new Point(22, 287);
+            lblEMail.Name = "lblEMail";
+            lblEMail.Size = new Size(63, 28);
+            lblEMail.TabIndex = 16;
+            lblEMail.Text = "Email:";
+            // 
+            // lblLName
+            // 
+            lblLName.AutoSize = true;
+            lblLName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblLName.ForeColor = Color.Black;
+            lblLName.Location = new Point(22, 175);
+            lblLName.Name = "lblLName";
+            lblLName.Size = new Size(103, 28);
+            lblLName.TabIndex = 15;
+            lblLName.Text = "Last name:";
+            // 
+            // lblFname
+            // 
+            lblFname.AutoSize = true;
+            lblFname.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFname.ForeColor = Color.Black;
+            lblFname.Location = new Point(22, 79);
+            lblFname.Name = "lblFname";
+            lblFname.Size = new Size(106, 28);
+            lblFname.TabIndex = 14;
+            lblFname.Text = "First name:";
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl1.ForeColor = Color.FromArgb(220, 204, 163);
+            lbl1.Location = new Point(21, 23);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(196, 31);
+            lbl1.TabIndex = 4;
+            lbl1.Text = "Search for a client\r\n";
+            // 
+            // lblCLients
+            // 
+            lblCLients.AutoSize = true;
+            lblCLients.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblCLients.ForeColor = Color.FromArgb(130, 76, 113);
+            lblCLients.Location = new Point(24, 9);
+            lblCLients.Name = "lblCLients";
+            lblCLients.Size = new Size(128, 46);
+            lblCLients.TabIndex = 3;
+            lblCLients.Text = "Clients";
             // 
             // Clients
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(233, 210, 224);
             ClientSize = new Size(1622, 907);
+            Controls.Add(lblCLients);
+            Controls.Add(pnlClient);
+            Controls.Add(flowLayoutPanel1);
             Name = "Clients";
             Text = "Clients";
+            pnlClient.ResumeLayout(false);
+            pnlClient.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel pnlClient;
+        private Label lblCLients;
+        private Label lbl1;
+        private Label lblEMail;
+        private Label lblLName;
+        private Label lblFname;
+        private TextBox txtEmailSearch;
+        private TextBox txtLNameSearch;
+        private TextBox txtFNameSearch;
+        private Button btnSearchClient;
     }
 }
--- BookHavenDesktop/Forms/MainPages/Employees.Designer.cs
+++ BookHavenDesktop/Forms/MainPages/Employees.Designer.cs
@@ -28,18 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            lblEmail = new Label();
+            lblLName = new Label();
+            lblFname = new Label();
+            txtEmailSearch = new TextBox();
+            txtLNameSearch = new TextBox();
+            txtFNameSearch = new TextBox();
+            btnSearchEmployee = new Button();
+            lbl1 = new Label();
+            lblEmployees = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.White;
+            flowLayoutPanel1.Location = new Point(432, -4);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(1194, 916);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(lblEmail);
+            panel1.Controls.Add(lblLName);
+            panel1.Controls.Add(lblFname);
+            panel1.Controls.Add(txtEmailSearch);
+            panel1.Controls.Add(txtLNameSearch);
+            panel1.Controls.Add(txtFNameSearch);
+            panel1.Controls.Add(btnSearchEmployee);
+            panel1.Controls.Add(lbl1);
+            panel1.Location = new Point(37, 72);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(355, 460);
+            panel1.TabIndex = 1;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblEmail.ForeColor = Color.Black;
+            lblEmail.Location = new Point(28, 282);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(63, 28);
+            lblEmail.TabIndex = 16;
+            lblEmail.Text = "Email:";
+            // 
+            // lblLName
+            // 
+            lblLName.AutoSize = true;
+            lblLName.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblLName.ForeColor = Color.Black;
+            lblLName.Location = new Point(28, 179);
+            lblLName.Name = "lblLName";
+            lblLName.Size = new Size(103, 28);
+            lblLName.TabIndex = 15;
+            lblLName.Text = "Last name:";
+            // 
+            // lblFname
+            // 
+            lblFname.AutoSize = true;
+            lblFname.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblFname.ForeColor = Color.Black;
+            lblFname.Location = new Point(28, 92);
+            lblFname.Name = "lblFname";
+            lblFname.Size = new Size(106, 28);
+            lblFname.TabIndex = 13;
+            lblFname.Text = "First name:";
+            // 
+            // txtEmailSearch
+            // 
+            txtEmailSearch.BackColor = Color.WhiteSmoke;
+            txtEmailSearch.BorderStyle = BorderStyle.None;
+            txtEmailSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtEmailSearch.Location = new Point(28, 313);
+            txtEmailSearch.Multiline = true;
+            txtEmailSearch.Name = "txtEmailSearch";
+            txtEmailSearch.Size = new Size(298, 41);
+            txtEmailSearch.TabIndex = 14;
+            // 
+            // txtLNameSearch
+            // 
+            txtLNameSearch.BackColor = Color.WhiteSmoke;
+            txtLNameSearch.BorderStyle = BorderStyle.None;
+            txtLNameSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtLNameSearch.Location = new Point(28, 210);
+            txtLNameSearch.Multiline = true;
+            txtLNameSearch.Name = "txtLNameSearch";
+            txtLNameSearch.Size = new Size(298, 41);
+            txtLNameSearch.TabIndex = 13;
+            // 
+            // txtFNameSearch
+            // 
+            txtFNameSearch.BackColor = Color.WhiteSmoke;
+            txtFNameSearch.BorderStyle = BorderStyle.None;
+            txtFNameSearch.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtFNameSearch.Location = new Point(28, 123);
+            txtFNameSearch.Multiline = true;
+            txtFNameSearch.Name = "txtFNameSearch";
+            txtFNameSearch.Size = new Size(298, 41);
+            txtFNameSearch.TabIndex = 10;
+            // 
+            // btnSearchEmployee
+            // 
+            btnSearchEmployee.BackColor = Color.FromArgb(130, 76, 113);
+            btnSearchEmployee.FlatAppearance.BorderSize = 0;
+            btnSearchEmployee.FlatStyle = FlatStyle.Flat;
+            btnSearchEmployee.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSearchEmployee.ForeColor = Color.White;
+            btnSearchEmployee.Location = new Point(28, 381);
+            btnSearchEmployee.Name = "btnSearchEmployee";
+            btnSearchEmployee.Size = new Size(298, 50);
+            btnSearchEmployee.TabIndex = 12;
+            btnSearchEmployee.Text = "Search employee";
+            btnSearchEmployee.UseVisualStyleBackColor = false;
+            // 
+            // lbl1
+            // 
+            lbl1.AutoSize = true;
+            lbl1.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbl1.ForeColor = Color.FromArgb(220, 204, 163);
+            lbl1.Location = new Point(15, 26);
+            lbl1.Name = "lbl1";
+            lbl1.Size = new Size(253, 31);
+            lbl1.TabIndex = 3;
+            lbl1.Text = "Search for an employee";
+            // 
+            // lblEmployees
+            // 
+            lblEmployees.AutoSize = true;
+            lblEmployees.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblEmployees.ForeColor = Color.FromArgb(130, 76, 113);
+            lblEmployees.Location = new Point(37, 9);
+            lblEmployees.Name = "lblEmployees";
+            lblEmployees.Size = new Size(190, 46);
+            lblEmployees.TabIndex = 2;
+            lblEmployees.Text = "Employees";
             // 
             // Employees
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(233, 210, 224);
             ClientSize = new Size(1622, 907);
+            Controls.Add(lblEmployees);
+            Controls.Add(panel1);
+            Controls.Add(flowLayoutPanel1);
             Name = "Employees";
             Text = "Employees";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private Label lbl1;
+        private Label lblEmployees;
+        private Button btnSearchEmployee;
+        private TextBox txtEmailSearch;
+        private TextBox txtLNameSearch;
+        private TextBox txtFNameSearch;
+        private Label lblFname;
+        private Label lblEmail;
+        private Label lblLName;
     }
 }
